cb03758756d05449525b34c93c12a2fd8ebe49b8
Anup Kumar <anup.kumar@kapsarc.org>
2017-07-30 14:36:14 +0300

    Added static pages to the respective routes and added the statics url path

--- enerMod.py
+++ enerMod.py
@@ -1,17 +1,30 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 import settings
 
 #Flask App
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/enermod/assets')
 app.config.from_object(settings)
 
-@app.route('/enermod/')
-def index():
-    return "EnerMod"
+@app.after_request
+def adding_header_content(head):
+    head.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    head.headers["Pragma"] = "no-cache"
+    head.headers["Expires"] = "0"
+    head.headers['Cache-Control'] = 'public, max-age=0'
+    return head
 
-@app.route('/enermod/login')
-def login():
-	return app.send_static_file('login.html')
+@app.route('/enermod/')
+@app.route('/enermod/<path:path>')
+def login(path=''):
+    return app.send_static_file('views/login.html')
+
+@app.route('/enermod/home')
+def home(path=''):
+    return app.send_static_file('views/home.html')
+
+@app.route('/enermod/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
 
 if __name__ == "__main__":
     try:
