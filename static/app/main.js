2b0ec120f40fd66553e237423ca712da46016809
Anup Kumar <anup.kumar@kapsarc.org>
2017-09-27 15:21:35 +0300

    Fixed the modal issue and added loading for ajax calls Added a new KAPSARC Loading gif

--- static/app/main.js
+++ static/app/main.js
@@ -1,5 +1,6 @@
 
 $(function() {
+    $('#loading').hide();
     //Page Tabs 
     var $tabs = $('.menu .item').tab();
     var handsontable
@@ -12,21 +13,23 @@ $(function() {
         }
         if (i == 1) {
             next = i + 2
-            $(this).append("<div class='column float-right'><button type='submit' class='ui button' id='next-tab' rel='" + next + "' sel='" + i + "'><i class='cloud upload icon'></i> Insert</button></div>");
+            $(this).append("<div class='column float-right'><button type='submit' class='ui button' id='insert-tab' rel='" + next + "' sel='" + i + "'><i class='cloud upload icon'></i> Insert</button></div>");
         }
-        if (i != 0) {
+        if (i != 0 && i != 2) {
             prev = i;
             $(this).append("<div class='column float-left'><button type='submit' class='ui button' id='prev-tab' rel='" + prev + "' sel='" + i + "'><i class='chevron circle left icon'></i> Previous</button></div>");
         }
     })
 
-    $('#next-tab, #prev-tab').click(function() { 
+    $('#next-tab, #insert-tab').click(function() {
+        var tab_proceed_status;
         if($(this).attr("sel") == 0){
             var cols = $('#selectedColumns').dropdown('get value')[$('#selectedColumns').dropdown('get value').length -1]
             var model = $('#selectedModel').find(':selected').val()
             var sm = $('#selectedSubModel').find(':selected').val()
             var fnname = $('#selectedFunction').find(':selected').val()
             if(validate(cols, model, sm, fnname)){
+                $('#loading').show();
                 $.ajax({
                     url: '/enermod/api/getTableData?cols='+$('#selectedColumns').dropdown('get value')[$('#selectedColumns').dropdown('get value').length -1]+'&model='+$('#selectedModel').find(":selected").val()+'&sm='+$('#selectedSubModel').find(':selected').val()+'&fnname='+$('#selectedFunction').find(':selected').val(),
                     type: 'GET',
@@ -48,47 +51,59 @@ $(function() {
                                     ]
                         };
                         handsontable = new Handsontable(handsontableElement, handsontableSettings);
+                        $('#loading').hide();
                     },
                     error: function(error) {
+                        $('#loading').hide();
                         console.log(error)
                     }
                 })
+                tabChange(tab_proceed_status=true, parseInt($(this).attr("sel")) + 2);
             }
             else
                 return;
         }
         if($(this).attr("sel") == 1) {
-            var approved=false;
-            //Popup Window
-            $('.ui.basic.modal')
-            .modal({
+            var num = $(this).attr("sel")
+            $('.ui.basic.modal').modal({
+                closable  : true,
                 onDeny: function(){
-                    approved=false;
+                    return;
                 },
                 onApprove : function() {
-                    approved=true;
+                    $('#loading').show();
+                    $.ajax({
+                        url: '/enermod/api/postToFact?editeddata='+JSON.stringify(handsontable.getData())+'&header='+JSON.stringify(tableHeader)+'&model='+$('#selectedModel').find(":selected").val()+'&sm='+$('#selectedSubModel').find(':selected').val()+'&fnname='+$('#selectedFunction').find(':selected').val()+'&mainversion='+$('#selectedVersion').find(':selected').val(),
+                        type: 'GET',
+                        success: function(response) {
+                                document.getElementById('clipboard').innerHTML += response;
+                        },
+                        error: function(error){
+                            $('#loading').hide();
+                            console.log(error);
+                        }
+                    })
+                    setTimeout(function(){
+                        $('#loading').hide();
+                        tabChange(tab_proceed_status=true, parseInt(num) + 2);
+                    }, 3000);
+                    return;
                 }
-              })
-            .modal('show');
-            if (approved){
-                $.ajax({
-                    url: '/enermod/api/postToFact?editeddata='+JSON.stringify(handsontable.getData())+'&header='+JSON.stringify(tableHeader)+'&model='+$('#selectedModel').find(":selected").val()+'&sm='+$('#selectedSubModel').find(':selected').val()+'&fnname='+$('#selectedFunction').find(':selected').val()+'&mainversion='+$('#selectedVersion').find(':selected').val(),
-                    type: 'GET',
-                    success: function(response) {
-                        console.log(response)
-                    }
-                })
-                console.log($('#selectedFunction').find(':selected').val())
-                console.log(handsontable.getData())
-                console.log(tableHeader)
-            }
-            else
-                return;
-            
+              }).modal('show');
         }
+    });
+
+    $('#prev-tab').click(function() {
         $tabs.tab('change tab', $(this).attr("rel"));
         return false;
-    });
+    })
+
+    function tabChange(status, tabnum) {
+        if (status) {
+            $tabs.tab('change tab', tabnum);
+            return false;   
+        }   
+    }    
     
     //Drop Down effect
     $('.ui.dropdown').dropdown({
