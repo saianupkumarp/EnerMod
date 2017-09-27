cb7ed003e0e3e9aa52bf0e32d00ac15eacfb068e
linahhamdan <linah.hamdan@kapsarc.org>
2017-09-27 08:34:21 +0300

    Fixed validation bug

--- static/app/main.js
+++ static/app/main.js
@@ -22,44 +22,68 @@ $(function() {
 
     $('#next-tab, #prev-tab').click(function() { 
         if($(this).attr("sel") == 0){
-            $.ajax({
-                url: '/enermod/api/getTableData?cols='+$('#selectedColumns').dropdown('get value')[$('#selectedColumns').dropdown('get value').length -1]+'&model='+$('#selectedModel').find(":selected").val()+'&sm='+$('#selectedSubModel').find(':selected').val()+'&fnname='+$('#selectedFunction').find(':selected').val(),
-                type: 'GET',
-                success: function(response) {
-                    tableHeader = response.colHeaders
-                    var handsontableElement = document.querySelector('#handsontable');
-                    var handsontableElementContainer = handsontableElement.parentNode;
-                    var handsontableSettings = {
-                                data: response.result,
-                                stretchH: 'all',
-                                width: '100%',
-                                height: 500,
-                                rowHeaders: true,
-                                colHeaders: response.colHeaders,
-                                columns: response.handsOnColumns,
-                                customBorders: [
-                                    {col: 3, left: {width: 2, color: 'red'},
-                                    right: {width: 1, color: 'green'}, top: '', bottom: ''}
-                                ]
-                    };
-                    handsontable = new Handsontable(handsontableElement, handsontableSettings);
-                },
-                error: function(error) {
-                    console.log(error)
-                }
-            })
+            var cols = $('#selectedColumns').dropdown('get value')[$('#selectedColumns').dropdown('get value').length -1]
+            var model = $('#selectedModel').find(':selected').val()
+            var sm = $('#selectedSubModel').find(':selected').val()
+            var fnname = $('#selectedFunction').find(':selected').val()
+            if(validate(cols, model, sm, fnname)){
+                $.ajax({
+                    url: '/enermod/api/getTableData?cols='+$('#selectedColumns').dropdown('get value')[$('#selectedColumns').dropdown('get value').length -1]+'&model='+$('#selectedModel').find(":selected").val()+'&sm='+$('#selectedSubModel').find(':selected').val()+'&fnname='+$('#selectedFunction').find(':selected').val(),
+                    type: 'GET',
+                    success: function(response) {
+                        tableHeader = response.colHeaders
+                        var handsontableElement = document.querySelector('#handsontable');
+                        var handsontableElementContainer = handsontableElement.parentNode;
+                        var handsontableSettings = {
+                                    data: response.result,
+                                    stretchH: 'all',
+                                    width: '100%',
+                                    height: 500,
+                                    rowHeaders: true,
+                                    colHeaders: response.colHeaders,
+                                    columns: response.handsOnColumns,
+                                    customBorders: [
+                                        {col: 3, left: {width: 2, color: 'red'},
+                                        right: {width: 1, color: 'green'}, top: '', bottom: ''}
+                                    ]
+                        };
+                        handsontable = new Handsontable(handsontableElement, handsontableSettings);
+                    },
+                    error: function(error) {
+                        console.log(error)
+                    }
+                })
+            }
+            else
+                return;
         }
         if($(this).attr("sel") == 1) {
-            $.ajax({
-                url: '/enermod/api/postToFact?editeddata='+JSON.stringify(handsontable.getData())+'&header='+JSON.stringify(tableHeader)+'&model='+$('#selectedModel').find(":selected").val()+'&sm='+$('#selectedSubModel').find(':selected').val()+'&fnname='+$('#selectedFunction').find(':selected').val()+'&mainversion='+$('#selectedVersion').find(':selected').val(),
-                type: 'GET',
-                success: function(response) {
-                    console.log(response)
+            var approved=false;
+            //Popup Window
+            $('.ui.basic.modal')
+            .modal({
+                onDeny: function(){
+                    approved=false;
+                },
+                onApprove : function() {
+                    approved=true;
                 }
-            })
-            console.log($('#selectedFunction').find(':selected').val())
-            console.log(handsontable.getData())
-            console.log(tableHeader)
+              })
+            .modal('show');
+            if (approved){
+                $.ajax({
+                    url: '/enermod/api/postToFact?editeddata='+JSON.stringify(handsontable.getData())+'&header='+JSON.stringify(tableHeader)+'&model='+$('#selectedModel').find(":selected").val()+'&sm='+$('#selectedSubModel').find(':selected').val()+'&fnname='+$('#selectedFunction').find(':selected').val()+'&mainversion='+$('#selectedVersion').find(':selected').val(),
+                    type: 'GET',
+                    success: function(response) {
+                        console.log(response)
+                    }
+                })
+                console.log($('#selectedFunction').find(':selected').val())
+                console.log(handsontable.getData())
+                console.log(tableHeader)
+            }
+            else
+                return;
             
         }
         $tabs.tab('change tab', $(this).attr("rel"));
@@ -77,6 +101,6 @@ $(function() {
         on: "click"
     });
 
-
+    
 
 });
